daa02c49e5ceaad9b3bff60c8d344efe535790ec
RGupta0323 <guptarohan323@gmail.com>
2023-06-13 21:14:55 -0500

    added api gateway, linked it to domain name, & created status_lambda

--- infra/cdk_stack.py
+++ infra/cdk_stack.py
@@ -1,9 +1,9 @@
-# can rename this file later. 
-import os 
+# can rename this file later.
 from constructs import Construct
-from . import config
+import boto3
 from aws_cdk import (Stack, Duration, aws_lambda, aws_dynamodb, aws_kms,
-                     aws_ec2 as ec2, aws_route53 as route53
+                     aws_ec2 as ec2, aws_route53 as route53, aws_apigateway as apigw,
+                    aws_certificatemanager as acm
                      )
 
 class AwsCommonServicesStack(Stack):
@@ -29,18 +29,34 @@ class AwsCommonServicesStack(Stack):
         domain_name = "thebrohan.net"
         domain_name_ns_servers = ["ns-1918.awsdns-47.co.uk", "ns-278.awsdns-34.com", "ns-1507.awsdns-60.org",
                                     "ns-686.awsdns-21.net"]
-        public_hosted_zone = route53.PublicHostedZone(self, "PublicHostedZone",
-                                 zone_name=domain_name
-                                 )
+        public_hosted_zone_id = "Z081614924Y6OKJ2LG5JH"
+        #public_hosted_zone = route53.HostedZone.from_lookup(self, "PublicHostedZone", domain_name=domain_name)
+        public_hosted_zone = route53.HostedZone.from_hosted_zone_attributes(self, "PublicHostedZone",
+                                                       zone_name=domain_name,
+                                                       hosted_zone_id=public_hosted_zone_id
+                                                       )
         route53.NsRecord(self, "NSRecord",
                          zone=public_hosted_zone,
                          record_name="BrohanNSRecord",
                          values=domain_name_ns_servers,
                          ttl=Duration.minutes(30)
                          )
-
+        acm_cert = acm.Certificate(self, f"{domain_name}_ACM_Certificate",
+                                    domain_name=domain_name,
+                                    certificate_name=f"{domain_name}_ACM_Certificate",
+                                    validation=acm.CertificateValidation.from_dns(public_hosted_zone)
+                                )
+        ### Generic Status Lambda to make sure api gateway is up and running ###
+        status_lambda = aws_lambda.Function(self, id="AWS-Common-Services-Status-Lambda",
+                                    code=aws_lambda.Code.from_asset("./software/src/lambda_functions/"),
+                                    handler="status_lambda.lambda_handler",
+                                    runtime=aws_lambda.Runtime.PYTHON_3_9)
         ### API Gateway ###
-
-        # domain name is going to hit this api gateway.
-        # the api gateway then will have links to all the apps you would like others to see (not all the apps need to dhave this)
-        # once this is done.
+        api = apigw.LambdaRestApi(self, "APIGateway",
+                                  domain_name=apigw.DomainNameOptions(
+                                    certificate=acm_cert,
+                                    domain_name=domain_name
+                                  ),
+                                  handler=status_lambda
+                                )
+        # WAF for API gateway
